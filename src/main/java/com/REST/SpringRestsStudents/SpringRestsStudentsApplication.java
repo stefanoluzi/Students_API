e21e6070caf00dd5286547827a3b46067cdec29b
stefanoluzi <stefanoluzi@hotmail.com>
2023-12-30 12:34:16 -0300

    First Commit

---
+++ src/main/java/com/REST/SpringRestsStudents/SpringRestsStudentsApplication.java
@@ -0,0 +1,13 @@
+package com.REST.SpringRestsStudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestsStudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestsStudentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/REST/SpringRestsStudents/SpringRestsStudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.REST.SpringRestsStudents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestsStudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
